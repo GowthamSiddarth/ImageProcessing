d2d701a296e5439e4fd7dd7b62453455e27db7f8
Gowtham Behara <gothsiddu@gmail.com>
2018-04-25 00:56:41 +0530

    Use haarcascade for face detection

---
+++ facedetection.py
@@ -0,0 +1,20 @@
+import cv2
+
+eye_cascade = cv2.CascadeClassifier('classifiers/haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('classifiers/haarcascade_frontalface_default.xml')
+
+img = cv2.imread('img/obama.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(image=gray, scaleFactor=1.3, minNeighbors=5)
+for x, y, w, h in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    roi_gray = gray[y:y + h, x:x + w]
+    roi_color = img[y:y + h, x:x + w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for ex, ey, ew, eh in eyes:
+        cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+cv2.imshow('gray', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
