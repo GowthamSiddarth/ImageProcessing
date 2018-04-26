a93ce258c58238a46f2281449ef160c1454c5a8f
Gowtham Behara <gothsiddu@gmail.com>
2018-04-26 09:49:29 +0530

    Calculate lower & upper threshold using median for Canny Edge Detection

--- cannyedgedetection.py
+++ cannyedgedetection.py
@@ -9,7 +9,17 @@ upper_red = np.array([255, 255, 180])
 
 mask = cv2.inRange(hsv_img, lowerb=lower_red, upperb=upper_red)
 res = cv2.bitwise_and(img, img, mask=mask)
-canny_edge = cv2.Canny(img, 100, 200)
+
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+median = np.median(gray_img)
+sigma = 0.33
+lower_threshold = int(max([0, (1 - sigma) * median]))
+upper_threshold = int(min([255, (1 + sigma) * median]))
+print("lower_threshold = " + str(lower_threshold))
+print("upper_threshold = " + str(upper_threshold))
+
+canny_edge = cv2.Canny(img, lower_threshold, upper_threshold)
 
 cv2.imshow('orig', img)
 cv2.imshow('hsv', hsv_img)
