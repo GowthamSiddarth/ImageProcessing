cd35d562e748e9b07aa5bf5b794d3928d70a5315
Gowtham Behara <gothsiddu@gmail.com>
2018-05-01 23:34:19 +0530

    Background reduction with MOG2 Background subtractor

---
+++ backgroundreduction.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+
+capture = cv2.VideoCapture(0)
+bg_subtractor = cv2.createBackgroundSubtractorMOG2()
+
+while True:
+    frame = capture.read()[1]
+
+    fg_mask = bg_subtractor.apply(frame)
+
+    cv2.imshow('orig', frame)
+    cv2.imshow('masked', fg_mask)
+
+    k = cv2.waitKey(30) & 0xFF
+    if k == 27:
+        break
+
+capture.release()
+cv2.destroyAllWindows()
