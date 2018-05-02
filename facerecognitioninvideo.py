c63feec28f548db0d811b54ed87fd1de5407e060
Gowtham Behara <gothsiddu@gmail.com>
2018-05-02 09:33:36 +0530

    Face Recognition Using Face Encodings

---
+++ facerecognitioninvideo.py
@@ -0,0 +1,55 @@
+import face_recognition
+import cv2
+
+video_capture = cv2.VideoCapture(0)
+
+barack_obama_img = cv2.imread('img/President_Barack_Obama.jpg')
+barack_obama_encoding = face_recognition.face_encodings(barack_obama_img)[0]
+
+my_img = cv2.imread('img/face.jpg')
+my_encoding = face_recognition.face_encodings(my_img)[0]
+
+known_face_encodings = [barack_obama_encoding, my_encoding]
+known_face_labels = ["Barack Obama", "Gowtham Siddarth"]
+
+process_this_frame = True
+
+while True:
+    frame = video_capture.read()[1]
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    if process_this_frame:
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = known_face_labels[first_match_index]
+
+            face_names.append(name)
+
+    process_this_frame = not process_this_frame
+
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        top = top * 4
+        right = right * 4
+        bottom = bottom * 4
+        left = left * 4
+
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.45, (255, 255, 255), 2)
+
+    cv2.imshow("Video", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
